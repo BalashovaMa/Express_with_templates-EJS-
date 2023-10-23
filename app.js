ed43545814c7d65ce0d2049752ae113d223acdfd
BalashovaMa <balashova.m.nd@gmail.com>
2023-10-23 16:20:23 +0300

    feat: add routes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello</h1>')
-})
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/restaurants', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'restaurants.html'));
+});
+
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/confirm', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'confirm.html'));
+});
+
+app.get('/recommend', function (req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'recommend.html'));
+});
 
 app.listen(3000);
