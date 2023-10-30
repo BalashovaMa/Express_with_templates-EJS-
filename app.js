1c6d564d6c37207d2ac520a2e507adc419fa7a87
BalashovaMa <balashova.m.nd@gmail.com>
2023-10-30 16:16:15 +0200

    refactor: add routes

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
-const uuid = require('uuid');
 
-const resData = require('./util/restaurant-data');
+const defaulRoutes = require('./routes/default');
+const restaurantRoutes = require('./routes/restaurants');
 
 const app = express();
 
@@ -14,49 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+app.use('/', defaulRoutes);
+app.use('/', restaurantRoutes);
 
-app.get('/restaurants', function (req, res) {
 
-    const storedRestaurants = resData.getStoredRestaurants();
 
-    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
-});
 
-app.get('/restaurants/:id', function (req, res) {
-    const restaurantId = req.params.id;
-    const storedRestaurants = resData.getStoredRestaurants();
-
-    for (const restaurant of storedRestaurants) {
-        if (restaurant.id === restaurantId) {
-            return res.render('restaurants-details', { restaurant: restaurant });
-        }
-    }
-    res.status(404).render('404');
-})
-
-app.get('/recommend', function (req, res) {
-    res.render('recommend');
-});
-
-app.post('/recommend', function (req, res) {
-    const restaurant = req.body;
-    restaurant.id = uuid.v4();
-    const restaurants = resData.getStoredRestaurants();
-    restaurants.push(restaurant);
-    resData.storeRestaurants(restaurants);
-    res.redirect('/confirm');
-});
-
-app.get('/confirm', function (req, res) {
-    res.render('confirm');
-});
-
-app.get('/about', function (req, res) {
-    res.render('about');
-});
 
 app.use(function (req, res) {
     res.status(404).render('404');
